0a5b7dcdbb574538283673c01dee40cac4617fe7
laekhole <113587338+laekhole@users.noreply.github.com>
2023-01-11 17:16:53 +0900

    [D1] Title: 1대1 가위바위보, Time: 88 ms, Memory: 42,724 KB -BaekjoonHub

---
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -0,0 +1,16 @@
+a,b = list(map(int,input().split()))
+if a==3:
+    if b==2:
+        print("A")
+    else:
+        print("B")
+if a==2:
+    if b==1:
+        print("A")
+    else:
+        print("B")
+if a==1:
+    if b==3:
+        print("A")
+    else:
+            print("B")
