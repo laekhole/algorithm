91ae27c46284c61c435bed30d759f995f7c5213d
laekhole <113587338+laekhole@users.noreply.github.com>
2023-01-18 01:51:10 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -0,0 +1,16 @@
+# 2562번 최댓값
+li=[]
+for i in range(9):
+    num=int(input())
+    li.append(num)
+st=li[0]
+cnt=1
+st_num=0
+for e in li:
+    if st<e:
+        st=e
+        st_num=cnt
+    cnt+=1
+print(st)
+# print(st_num)
+print(li.index(max(li))+1)
