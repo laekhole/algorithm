64ccc34733898c885f6f09bce0dbeeafb1b7eb5d
laekhole <113587338+laekhole@users.noreply.github.com>
2023-01-26 11:47:31 +0900

    [Silver V] Title: 카드1, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,16 +1,11 @@
-# 백준 2161 카드1
 N=int(input())
 li=[i for i in range(1,N+1)]
 ans=[]
-# print(li)
 for ind in range(N):
-    # print(li[0])
     ans.append(li[0])
     try:
         li.pop(0)
-        # print(li)
         li.append(li.pop(0))
-        # print(li)
     except:
         pass
 print(*ans)
