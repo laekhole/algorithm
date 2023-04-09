5c15d2979c3c4fc3758a84a785a980fb7048f468
laekhole <113587338+laekhole@users.noreply.github.com>
2023-04-09 22:21:08 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,2 +1,8 @@
-a, b = input().rstrip().split()
-print(int(a, int(b)))
+n,b=input().split()
+b=int(b)
+# print(n,b)
+notation="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+result=0
+for _ in range(len(n)):
+    result+=notation.index(n[_])*(b**(len(n)-1-_))
+print(result)
